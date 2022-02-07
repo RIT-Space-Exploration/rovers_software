da9737a3443c268c022d97000fbfee0fd39729a0
Sean Clifford <smc6548@rit.edu>
2022-02-07 19:14:56 +0000

    fixed initialize? checkout should work now

--- scripts/initialize.bash
+++ scripts/initialize.bash
@@ -3,11 +3,12 @@ sudo apt update
 export RMW_IMPLEMENT ATION=rmw_fastrtps_cpp
 
 git fetch origin galactic
-git checkout --track -b dev_galactic || git checkout --track dev_galactic
+git checkout --track -b dev_galactic || git checkout galactic
 
 rosdep update
 rosdep install --from-paths src --ignore-src -r -y
 vcs import src < spex_rover.repos
+
 
 colcon build --symlink-install
 . ./install/local_setup.bash
