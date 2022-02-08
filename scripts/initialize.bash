83326d6071bc19987a063aa0504432245be31bc6
Sean Clifford <smc6548@rit.edu>
2022-02-07 21:40:24 -0500

    updated COLCON_IGNORE and added usb passthrought to the container

--- scripts/initialize.bash
+++ scripts/initialize.bash
@@ -3,7 +3,7 @@ sudo apt update
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 
 git fetch origin galactic
-git checkout --track -b dev_galactic origin/galactic || git checkout --track  origin/galactic
+git checkout -b dev_galactic origin/galactic || git checkout origin/galactic
 
 rosdep update
 rosdep install --from-paths src --ignore-src -r -y
