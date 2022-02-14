6534dd4fa6cb803636b1a09c15d3e810af5e30f4
Sean Clifford <smc6548@rit.edu>
2022-02-14 16:46:57 +0000

    added moveit changes and fixed setup

--- scripts/initialize.bash
+++ scripts/initialize.bash
@@ -1,14 +1,20 @@
 sudo apt update
+sudo apt dist-upgrade
 
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 
 git fetch origin galactic
 git checkout -b dev_galactic origin/galactic || git checkout origin/galactic
 
+sudo apt install python3-colcon-mixin
+colcon mixin add default https://raw.githubusercontent.com/colcon/colcon-mixin-repository/master/index.yaml
+colcon mixin update default
+
 rosdep update
-rosdep install --from-paths src --ignore-src -r -y
+rosdep install --from-paths . --ignore-src -r --rosdistro galactic -y
 vcs import src < spex_rover.repos
 
 
 colcon build --symlink-install
-. ./install/local_setup.bash
+source ./install/setup.bash
+echo 'source ~/spex_rover/install/setup.bash' >> ~/.bashrc
